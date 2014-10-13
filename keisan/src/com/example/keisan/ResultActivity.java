d6cf5838e268158a9abd972965d41d65184e3c8d
akimicyu <akimicyu@gmail.com>
2014-10-13 23:18:40 +0900

    ハイスコア周りを追加(プリファレンスによる永続化付き)

--- keisan/src/com/example/keisan/ResultActivity.java
+++ keisan/src/com/example/keisan/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.example.keisan;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -24,6 +26,15 @@ public class ResultActivity extends Activity {
 			TextView percent = ((TextView) findViewById(R.id.textViewPercent));
 			float per = 100.0f * countCorrect / countAnswer;
 			percent.setText(getResources().getString(R.string.percent, per));
+			// 以降、ハイスコア周り
+			SharedPreferences sp = getSharedPreferences("config", Context.MODE_PRIVATE);
+			int highScore = sp.getInt("highScore", 0);
+			if (highScore < countCorrect) {
+				sp.edit().putInt("highScore", countCorrect).commit();
+				TextView hvhs = ((TextView) findViewById(R.id.textViewHighScoreUpdate));
+				hvhs.setText(getResources().getString(R.string.highScoreUpdate, highScore, countCorrect));
+				hvhs.setVisibility(View.VISIBLE);
+			}
 		}
 		Button bn = (Button) findViewById(R.id.buttonRetry);
 		bn.setOnClickListener(new OnClickListener() {
--- keisan/src/com/example/keisan/TitleActivity.java
+++ keisan/src/com/example/keisan/TitleActivity.java
@@ -1,11 +1,14 @@
 package com.example.keisan;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class TitleActivity extends Activity {
 	
@@ -28,6 +31,16 @@ public class TitleActivity extends Activity {
 				gameStart("Endless", MainActivity.ENDLESS_END);
 			}
 		});
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// ハイスコア表示
+		SharedPreferences sp = getSharedPreferences("config", Context.MODE_PRIVATE);
+		int highScore = sp.getInt("highScore", 0);
+		TextView highScoreText = ((TextView) findViewById(R.id.textViewHighScore));
+		highScoreText.setText(getResources().getString(R.string.highScore, highScore));
 	}
 
 	private void gameStart(String mode, int requestcode) {
